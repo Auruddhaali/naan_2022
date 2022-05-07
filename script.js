9979b13887a0f11a8261f7319a3ff80500d0ba8b
Auruddhaali <92882285+Auruddhaali@users.noreply.github.com>
2022-05-08 00:23:48 +0600

    Update script.js

--- script.js
+++ script.js
@@ -294,7 +294,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "https://www.youtube.com/embed/YDzn9fddPUs?controls=0&loop=1&autoplay=1");
+  ifrm.setAttribute("src", "https://www.youtube.com/embed/RQ-h6WCVm6Q?controls=0&loop=1&autoplay=1");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
